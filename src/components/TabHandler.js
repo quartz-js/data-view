468cbea3882b03906c5ab7e1522df156f818f7b2
Railken <railken@protonmail.ch>
2020-09-09 00:39:45 +0200

    comments

--- src/components/TabHandler.js
+++ src/components/TabHandler.js
@@ -1,5 +1,8 @@
 import { Url } from '@quartz/core'
 
+/**
+ * Handle tab and store the value of the selected tab in query url
+ */
 export var TabHandler = {
   data: function() {
     return {
@@ -11,16 +14,32 @@ export var TabHandler = {
     this.updateUrlQueryParameter(this.tabs);
   },
   methods: {
+
+    /**
+     * Update current tab value from the url
+     */
     updateTabFromQueryUrl() {
       this.tabs = parseInt(this.$route.query[this.getTabKey()] || 0)
     },
+
+    /**
+     * Get the key used to identify the parameter in the url
+     */
     getTabKey() {
       return this.manager.name + ".tab"
     },
+
+    /**
+     * Called when the value of the tab is changed
+     */
     onTabChange(e) {
       this.tabs = e
       this.updateUrlQueryParameter(e);
     },
+
+    /**
+     * Update the url query string
+     */
     updateUrlQueryParameter(value) {
       Url.updateQueryUrlParameter(this.getTabKey(), value)
     }
